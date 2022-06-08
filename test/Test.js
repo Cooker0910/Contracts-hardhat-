6dbb2a15996424e076b3159b7bd5974ede42cef5
Caesar0910 <cooker98910@gmail.com>
2022-06-08 09:02:15 +0900

    update

--- test/Test.js
+++ test/Test.js
@@ -1,16 +1,21 @@
 const { expect } = require("chai");
 
 describe("Test contract", function () {
-  it("Addition should work", async function () {
-    // Create the smart contract object to test from
-    const [owner] = await ethers.getSigners();
-    const TestContract = await ethers.getContractFactory("Test");
-    const contract = await TestContract.deploy();
+    let contract;
+    let owner;
 
-    // Get output from functions
-    const additionTest = await contract.add(6, 6);
-    const subtractionTest = await contract.subtract(6, 6);
-    expect(additionTest).to.equal(12);
-    expect(subtractionTest).to.equal(0);
-  });
+    beforeEach(async function () {
+        // Create the smart contract object to test from
+        [owner] = await ethers.getSigners();
+        const TestContract = await ethers.getContractFactory("Test");
+        contract = await TestContract.deploy();
+    });
+
+    it("Addition should work", async function () {
+        // Get output from functions
+        const additionTest = await contract.add(6, 6);
+        const subtractionTest = await contract.subtract(6, 6);
+        expect(additionTest).to.equal(12);
+        expect(subtractionTest).to.equal(0);
+    });
 });
